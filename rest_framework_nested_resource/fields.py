b209213b8d0cd6131515822a71587ec599da8a3a
Joakim Soderlund <joakim.soderlund@gmail.com>
2015-04-28 12:11:26 +0200

    Added default for parent content type.

--- rest_framework_nested_resource/fields.py
+++ rest_framework_nested_resource/fields.py
@@ -20,3 +20,16 @@ class ParentPrimaryKeyDefault():
 
     def __call__(self):
         return self.parent.pk
+
+
+class ParentContentTypeDefault():
+    """
+    Returns the parent content type from the nested resource view.
+    """
+
+    def set_context(self, serializer_field):
+        self.parent = serializer_field.context['parent']
+
+    def __call__(self):
+        from django.contrib.contenttypes.models import ContentType
+        return ContentType.objects.get_for_model(self.parent)
